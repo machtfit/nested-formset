e7514f38d8bd7f04880952c579791d556c2d6dd4
Jonathan Fiebelkorn <jonathan@machtfit.de>
2014-07-30 15:53:43 +0200

    Tag custom version 0.1.1.99

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 
-version = '0.1.1'
+version = '0.1.1.99'
 
 
 setup(name='django-nested-formset',
